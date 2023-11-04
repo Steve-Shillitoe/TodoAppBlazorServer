d8a1c26836836b95bdc7286e1a82f50628190948
IMI-TRISTAN <s.shillitoe@sheffield.ac.uk>
2023-11-04 00:26:08 +0000

    Add project files.

---
+++ TodoAppBlazorServer/Services/TodoService.cs
@@ -0,0 +1,41 @@
+﻿namespace TodoAppBlazorServer.Services
+{
+    public class TodoService : ITodoService
+    {
+        private readonly IList<TodoItem> _todoItems;
+        public TodoService()
+        {
+            _todoItems = new List<TodoItem>()
+            { 
+                new TodoItem("Feed Ferrets"),
+                new TodoItem("Learn Blazor")
+            };
+        }
+
+        public void Add(TodoItem item) 
+        { 
+            _todoItems.Add(item);
+        }
+
+        public IEnumerable<TodoItem> GetAll() 
+        {
+            return _todoItems.ToList();
+        }
+
+        public void Delete(TodoItem item)
+        {
+            _todoItems.Remove(item);
+        }
+
+        public void Complete(TodoItem item)
+        {
+            item.Completed = true;
+        }
+
+        public void Uncomplete(TodoItem item)
+        {
+            item.Completed = false;
+        }
+
+    }
+}
---
+++ TodoAppBlazorServer/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿namespace TodoAppBlazorServer
+{
+    public class TodoItem  //Data class
+    {
+        public TodoItem(string text)  //Constructor
+        {
+            Text = text;
+        }
+        public string Text { get; set; } = String.Empty;
+        public bool Completed { get; set; }  = false;
+    }
+}
